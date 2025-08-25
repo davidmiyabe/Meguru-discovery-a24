8fe7d8215bfab04c8976789bae7cc96306ed103a
David Miyabe <davidmiyabe@users.noreply.github.com>
2025-08-24 22:05:25 -0700

    Import useToast in draft route

--- apps/web/src/routes/draft.tsx
+++ apps/web/src/routes/draft.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import { useToast } from '../components/ToastProvider'
 import { createDraftItinerary } from '../lib/services/itinerary'
 import { saveTrip } from '../lib/services/trip'
 import { useItineraryStore } from '../stores/itineraryStore'
